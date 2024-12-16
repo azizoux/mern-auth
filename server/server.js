f6fb3e07227263aa0e30a19a5c5ecee34094d1b2
Abdelaziz Mahamat Ali <azizmahamat@gmail.com>
2024-12-16 08:49:48 +0100

    setup auth functions

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 connectDB();
+
+//API Endpoints
+app.get("/", (req, res) => res.send("API working"));
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server started on PORT ${port} ...`);
